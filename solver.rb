dfc0c4da11f90a62e05e3b411acdb6f08d6f8c33
SekoVper <damehlivingstone1@gmail.com>
2023-02-24 15:32:21 +0000

    Add factorial method for testing.

--- solver.rb
+++ solver.rb
@@ -1,3 +1,8 @@
 class Solver
-  
+  def factorial(num)
+    raise ArgumentError, 'n must be a non-negative integer' unless num.is_a?(Integer) && num >= 0
+    return 1 if num.zero?
+
+    (1..num).reduce(:*)
+  end
 end
