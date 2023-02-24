245c036bebad5d485571f37b0e07dd82b632ee28
SekoVper <damehlivingstone1@gmail.com>
2023-02-24 15:31:37 +0000

    Modify code

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,20 +4,21 @@ describe Solver do
   solver = Solver.new
   context 'When given a positive integer' do
     it 'Returns the correct factorial value' do
-      expect (solver.factorial(0)).to eq(1)
-      expect (solver.factorial(1)).to eq(2)
-      expect (solver.factorial(2)).to eq(6)
+      expect(solver.factorial(0)).to eq(1)
+      expect(solver.factorial(1)).to eq(1)
+      expect(solver.factorial(3)).to eq(6)
     end
   end
+
   context 'When given a negative integer' do
     it 'Raises an exception' do
-      expect {solver.factorial(-1)}.to raise_error(ArgumentError)
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
   context 'When giving a non integer value' do
     it 'Raise an exception' do
-      expect {solver.factorial(2.5)}.to raise_error(ArgumentError)
-      expect {solver.factorial('Umair')}.to raise_error(ArgumentError)
+      expect { solver.factorial(2.5) }.to raise_error(ArgumentError)
+      expect { solver.factorial('Umair') }.to raise_error(ArgumentError)
     end
   end
 end
