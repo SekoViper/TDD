d08ddb7f5a20a780e2fee93002e91173d257ea72
SekoVper <damehlivingstone1@gmail.com>
2023-02-24 16:29:35 +0000

    Add fizzbuz Method for testing

--- solver.rb
+++ solver.rb
@@ -11,4 +11,18 @@ class Solver
 
     word.reverse
   end
+
+  def fizz_buzz(num)
+    raise ArgumentError, 'Input must be an integer' if !num.is_a?(Integer) || num.negative?
+
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
